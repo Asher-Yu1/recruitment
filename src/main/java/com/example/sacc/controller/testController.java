8d52fc5eea86859d4b9de9af010e2c905212bb5d
Asher-Yu1 <138885017+Asher-Yu1@users.noreply.github.com>
2023-09-06 15:13:26 +0800

    新增post接口

--- src/main/java/com/example/sacc/controller/testController.java
+++ src/main/java/com/example/sacc/controller/testController.java
@@ -5,6 +5,7 @@ import com.example.sacc.dao.Teacher;
 import com.example.sacc.mapper.StudentMapper;
 import com.example.sacc.mapper.TeacherMapper;
 import jakarta.annotation.Resource;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,10 +16,27 @@ public class testController {
     @Resource
     private TeacherMapper teacherMapper;
 
-    @RequestMapping ("/test/{id}")
+    @GetMapping ("/test/{id}")
     public String test(@PathVariable(value = "id")Integer id){
         Student student = studentMapper.selectById(id);
         Teacher teacher = teacherMapper.selectById(student.getTeacherId());
-        return student.getId()+"  "+student.getName()+"的老师是"+teacher.getName();
+        return student.getName()+"的老师是"+teacher.getName();
+    }
+
+    @PostMapping("/addTeacher")
+    public void add(@Param("name")String name,@Param("id")Integer id){
+        Teacher teacher = new Teacher();
+        teacher.setId(id);
+        teacher.setName(name);
+        teacherMapper.insert(teacher);
+    }
+
+    @PostMapping("/addStudent")
+    public void addStudent(@Param("name")String name,@Param("id")Integer id,@Param("teacherId")Integer teacherId){
+        Student s = new Student();
+        s.setId(id);
+        s.setName(name);
+        s.setTeacherId(teacherId);
+       studentMapper.insert(s);
     }
 }
